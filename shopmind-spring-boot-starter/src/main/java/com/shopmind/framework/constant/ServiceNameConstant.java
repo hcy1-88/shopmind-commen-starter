ff65217427553fa0d6d6ee4cd78d98b76a471d08
hcy180 <hcy1805110063@163.com>
2025-12-18 20:25:10 +0800

    服务名添加shopmind前缀

--- shopmind-spring-boot-starter/src/main/java/com/shopmind/framework/constant/ServiceNameConstant.java
+++ shopmind-spring-boot-starter/src/main/java/com/shopmind/framework/constant/ServiceNameConstant.java
@@ -6,9 +6,10 @@ package com.shopmind.framework.constant;
  * Date: 2025-12-18
  */
 public interface ServiceNameConstant {
-    String AUTH_SERVICE = "auth-service";
-    String USER_SERVICE = "user-service";
-    String ORDER_SERVICE = "order-service";
-    String PRODUCT_SERVICE = "product-service";
-    String COMMENT_SERVICE = "comment-service";
+    String GATEWAY = "shopmind-gateway";
+    String AUTH_SERVICE = "shopmind-auth-service";
+    String USER_SERVICE = "shopmind-user-service";
+    String ORDER_SERVICE = "shopmind-order-service";
+    String PRODUCT_SERVICE = "shopmind-product-service";
+    String COMMENT_SERVICE = "shopmind-comment-service";
 }
