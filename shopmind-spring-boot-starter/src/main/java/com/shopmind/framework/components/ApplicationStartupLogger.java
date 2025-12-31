2f6bf1343a3105face63fbfffa34b50ad6b74ef8
hcy180 <hcy1805110063@163.com>
2025-12-31 12:14:39 +0800

    美观日志2

--- shopmind-spring-boot-starter/src/main/java/com/shopmind/framework/components/ApplicationStartupLogger.java
+++ shopmind-spring-boot-starter/src/main/java/com/shopmind/framework/components/ApplicationStartupLogger.java
@@ -59,7 +59,7 @@ public class ApplicationStartupLogger implements ApplicationListener<Application
               .append("║                                                                      \n")
               .append(String.format("║   • 应用名称：%-56s\n", appName))
               .append(String.format("║   • 激活环境：%-56s\n", profile))
-              .append(String.format("║   • 服务端口：%-56s║", port))
+              .append(String.format("║   • 服务端口：%-56s\n", port))
               .append(String.format("║   • 主机地址：%-56s\n", baseUrl))
               .append(String.format("║   • 启动耗时：%-56s\n", String.format("%.2f 秒", seconds)))
               .append("║                                                                      \n")
